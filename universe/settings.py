5153ed4f73976248088d37ef9f1fcb2229437a4f
Tariq Ebden <tebden16@gmail.com>
2024-03-04 13:22:50 +0000

    updated allowed hosts

--- universe/settings.py
+++ universe/settings.py
@@ -27,7 +27,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
   '8000-tariq845-universe-h2qtzgq4fi1.ws-eu108.gitpod.io',
-  '.heroku.com',
+  '.herokuapp.com',
   'https://universe-c670bf4af548.herokuapp.com/'
 ]
 

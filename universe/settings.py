57dfa3d39e642796a96cf9e66d623b00efd4aabf
Tariq Ebden <tebden16@gmail.com>
2024-03-04 16:27:44 +0000

    fixed debug settings

--- universe/settings.py
+++ universe/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
   '8000-tariq845-universe-h2qtzgq4fi1.ws-eu108.gitpod.io',

b729bffefce42f76ba975ba597007d12343fc560
Tariq Ebden <tebden16@gmail.com>
2024-03-05 15:47:09 +0000

    added authentication

--- event/models.py
+++ event/models.py
@@ -5,8 +5,12 @@ from cloudinary.models import CloudinaryField
 # Create your models here.
 class Event(models.Model):
   name = models.CharField(max_length=200, unique=True)
+  slug = models.SlugField(max_length=200, unique=True)
   description = models.TextField()
   image = CloudinaryField('image', default='placeholder')
   date = models.DateTimeField(auto_now_add = True)
   location = models.CharField(max_length=200)
   max_participants = models.IntegerField(200)
+
+  def __str__(self):
+    return f''

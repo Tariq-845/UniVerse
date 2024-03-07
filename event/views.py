724e18eb0be74788d44d2074951b7f2cff1cdf89
Tariq Ebden <tebden16@gmail.com>
2024-03-07 23:42:05 +0000

    added forms to allow students to post reviews

--- event/views.py
+++ event/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
+from django.contrib import messages
 from .models import Event, Review
+from .forms import ReviewForm
 
 # Create your views here.
 class Events(generic.ListView):
@@ -14,6 +16,20 @@ def event_detail(request, slug):
   reviews = event.reviews.all().order_by("-date_posted")
   review_count = event.reviews.all().count()
 
+  if request.method == "POST":
+    review_form = ReviewForm(data=request.POST)
+    if review_form.is_valid():
+      review = review_form.save(commit=False)
+      review.author = request.user
+      review.event = event
+      review.save()
+      messages.add_message(
+        request, messages.SUCCESS,
+        'Your review has successfully been posted!'
+      )
+
+  review_form = ReviewForm()
+
   return render(
     request,
     "events/event_detail.html",
@@ -21,5 +37,6 @@ def event_detail(request, slug):
       "event": event,
       "reviews": reviews,
       "review_count": review_count,
+      "review_form": review_form,
     }
   )
